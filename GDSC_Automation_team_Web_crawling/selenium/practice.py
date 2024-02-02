cd405b3bae65ff23af09fe06b1847f10f4bb1915
tlgusdl03 <tlgusdl03@gmail.com>
2024-02-02 12:10:31 +0900

    fix: 코드 정리

--- GDSC_Automation_team_Web_crawling/selenium/practice.py
+++ GDSC_Automation_team_Web_crawling/selenium/practice.py
@@ -29,16 +29,13 @@ driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 10)
 
 driver.implicitly_wait(10)
-print("웹 드라이버 초기화")
 
 # 웹사이트 열기
 driver.get('https://plaza.inha.ac.kr/plaza/2091/subview.do?enc=Zm5jdDF8QEB8JTJGcGxhemFCYnMlMkZwbGF6YSUyRjEwMDAxJTJGYXJ0Y2xMaXN0LmRvJTNG')
-print("웹사이트 열기")
 
 # 로그인 버튼 클릭
 input_element = driver.find_element(By.XPATH, '//*[@id="menu2091_obj16"]/form/div/div/div[2]/div/span/input')
 input_element.click()
-print("로그인 버튼 클릭")
 
 # 아이디, 비밀번호 입력
 input_id = driver.find_element(By.XPATH, '/html/body/div[6]/div/div[2]/div/article/div/form/div/div[1]/dl[1]/dd/input')
@@ -47,7 +44,6 @@ input_pw = driver.find_element(By.XPATH, '/html/body/div[6]/div/div[2]/div/artic
 input_id.send_keys(id)
 input_pw.send_keys(pw)
 input_pw.send_keys(Keys.RETURN)
-print("아이디 비밀번호 입력")
 sleep(1)
 
 # 제목으로 정렬, 공모전 키워드만 검색
@@ -57,7 +53,6 @@ select_object = Select(select)
 select_object.select_by_value('sj')
 search.send_keys('공모전')
 search.send_keys(Keys.RETURN)
-print("공모전만 검색")
 sleep(1)
 
 # flag 설정
@@ -74,15 +69,11 @@ while True:
     # 각 게시물들의 리스트를 얻음
     Eventitles = driver.find_elements(By.CLASS_NAME, '_artclEven')
     Oddtitles = driver.find_elements(By.CLASS_NAME, '_artclOdd')
-    print("게시물 리스트 얻음")
 
     # 게시물 안의 요소에 접근
     for i in Eventitles:
-        sleep(1)
         date = i.find_element(By.XPATH, './td[5]').text # 작성일을 확인하여 6개월 이전에 작성된 경우 데이터 수집 종료
         converted_time = datetime.strptime(date, '%Y.%m.%d.')
-        print("Eventitles`s date : " , date)
-        print("Eventitles`s date : " , converted_time)
 
         if converted_time < six_months_ago:
             print("6개월 이전 게시물")
@@ -96,17 +87,11 @@ while True:
             up = i.find_element(By.XPATH, './td[6]').text # 좋아요
             down = i.find_element(By.XPATH, './td[7]').text # 싫어요
             access = i.find_element(By.XPATH, './td[8]').text # 조회수
-            print(category, title, up, down, access, href)
             ws.append([category, title, up, down, access, href])
-            print("추가함")
 
     for j in Oddtitles:
-
-        sleep(1)
         date = j.find_element(By.XPATH, './td[5]').text # 작성일을 확인하여 6개월 이전에 작성된 경우 데이터 수집 종료
         converted_time = datetime.strptime(date, '%Y.%m.%d.')
-        print("Oddtitles`s date : " , date)
-        print("Oddtitles`s date : " , converted_time)
 
         if converted_time < six_months_ago:
             print("6개월 이전 게시물")
@@ -120,9 +105,7 @@ while True:
             up = j.find_element(By.XPATH, './td[6]').text # 좋아요
             down = j.find_element(By.XPATH, './td[7]').text # 싫어요
             access = j.find_element(By.XPATH, './td[8]').text # 조회수
-            print(category, title, up, down, access, href)
             ws.append([category, title, up, down, access, href])
-            print("추가함")
 
     # 다음 페이지로 넘어감
     try:
@@ -133,11 +116,9 @@ while True:
         next_button = driver.find_element(By.XPATH, '/html/body/div[6]/div/div[2]/div/article/div[2]/div[2]/form[3]/div/div/ul/li[' + str(page_index) + ']')
         page_index = 1
         next_button.click()
-        print("다음 페이지")
         sleep(1)
     else:
         next_button.click()
-        print("다음 페이지")
         sleep(1)
 
 wb.save('test.xlsx')
---
+++ README.md
@@ -0,0 +1,22 @@
+practice.py와 같은 폴더내에 data.py파일을 생성 후 
+
+userid = '본인의 아이디'
+userpassword = '본인의 비밀번호'
+
+위의 내용을 따옴표 포함하여 추가해 주세요, 내용은 본인것으로 교체해야 합니다.
+
+이후 practice.py를 실행시켜주세요
+
+
+실행이 안될시 터미널을 열어
+
+pip install selenium
+pip install openpyxl
+pip install python-dateutil
+
+라이브러리를 설치한 후
+
+https://googlechromelabs.github.io/chrome-for-testing/
+
+위의 링크로 접속 또는 다른 방법을 통해서 본인 크롬의 버전과 같은 chormedriver를 설치하여 
+chromedriver.exe 파일을 selenium 폴더안에 위치한 후 실행시켜 주세요
